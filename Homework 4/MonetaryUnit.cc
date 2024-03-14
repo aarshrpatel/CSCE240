67e852f747dd9746103a9887cbb747f4fae212e3
Aarsh Patel <aarshpatel2004@gmail.com>
2024-03-14 11:47:26 -0400

    Add Homework 4

--- Homework 4/MonetaryUnit.cc
+++ Homework 4/MonetaryUnit.cc
@@ -1,6 +1,44 @@
 // Copyright 2024 Aarsh Patel
 #include<iostream>
+using std::cout;
+using std::endl;
+using std::ostream;
+#include<string>
+using std::string;
+#include"MonetaryUnit.h"
 
-class MonetaryUnit:
-  public:
-    
+
+// ostream& operator << (ostream& whereto, const MonetaryUnit& mu) {
+//   whereto << mu._unit_name << " " << mu._unit_symbol << "" << mu._unit_amount;
+//   return whereto;
+// }
+
+bool MonetaryUnit::SetName(string name) {
+  if ( name.length() >= 1 ) {
+    _unit_name = name;
+  }
+  return _unit_name == name;
+}
+
+bool MonetaryUnit::SetSymbol(string symbol) {
+  if ( symbol.length() >= 1 ) {
+    _unit_symbol = symbol;
+  }
+  return _unit_symbol == symbol;
+}
+
+bool MonetaryUnit::SetAmountEquivalentTo1Dollar(double amount) {
+  if ( amount > 0 ) {
+    _unit_amount = amount;
+  }
+  return _unit_amount == amount;
+}
+
+
+
+MonetaryUnit::MonetaryUnit(string name, string symbol, double amount) : 
+            _unit_name("US dollars"), _unit_symbol("$"), _unit_amount(1){
+  SetName(name);
+  SetSymbol(symbol);
+  SetAmountEquivalentTo1Dollar(amount);
+}
--- Homework 4/MonetaryUnit.h
+++ Homework 4/MonetaryUnit.h
@@ -1,7 +1,26 @@
 // Copyright 2024 Aarsh Patel
 #ifndef _MONETARYUNIT_H_
 #define _MONETARYUNIT_H_
+#include<iostream>
+using std::ostream;
+#include<string>
+using std::string;
+class MonetaryUnit {
+//   friend ostream& operator << (ostream&, const MonetaryUnit&);
+ public:
+  explicit MonetaryUnit(string name = "US dollars", string symbol = "$", double amount = 1);
+  
+  ~MonetaryUnit() { std::cout << "Destroying MonetaryUnit Class" << std::endl; }
+  bool SetName(string name);
+  string GetName() const { return _unit_name; }
+  bool SetSymbol(string symbol);
+  string GetSymbol() const { return _unit_symbol; }
+  bool SetAmountEquivalentTo1Dollar(double amount);
+  double GetAmountEquivalentTo1Dollar() const { return _unit_amount; }
 
-
-
+ private:
+  string _unit_name;
+  string _unit_symbol;
+  double _unit_amount;
+};
 #endif 
