a6b52e6c177bcbb75f1f3def8e0f695bccc7c0bd
Aarsh Patel <142952200+aarshrpatel@users.noreply.github.com>
2024-04-09 19:50:38 +0000

    create StreamingTrack.cc

---
+++ Homework 5/StreamingTrack.cc
@@ -0,0 +1,13 @@
+// Copyright 2024 Aarsh Patel
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <string>
+using std::string;
+#include "StreamingTrack.h"
+
+namespace csce240_programming_assignment_5 {
+ class StreamingTrack : public SongRecording {
+
+ };
+}  // namespace csce240_programming_assignment_5
