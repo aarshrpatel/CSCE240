c8a53d6a1a52b9794ceb25c8e74157ca7a3f5873
Aarsh Patel <142952200+aarshrpatel@users.noreply.github.com>
2024-04-10 14:56:12 +0000

    Fix Styling SongRecording

--- Homework 5/SongRecording.cc
+++ Homework 5/SongRecording.cc
@@ -4,89 +4,88 @@
 using std::string;
 #include "SongRecording.h"
 
-
 namespace csce240_programming_assignment_5 {
 
-// Default Constructor
-SongRecording::SongRecording(const string& title,
- const string& primary_artist, int duration, int num_artists) :
- title_(title), num_artists_(num_artists > 0 ? num_artists : 1), duration_(duration) {
-  artists_ = new string[num_artists_];
-  SetArtist(primary_artist);
-  for (int i = 1; i < num_artists_; i++) {
-    artists_[i] = "unknown";
-  }
-}
-
-// Copy Constructor
-SongRecording::SongRecording(const SongRecording& other_song) :
- title_(other_song.title_), num_artists_(other_song.num_artists_),
- duration_(other_song.duration_) {
-  artists_ = new string[num_artists_];
-  for (int i = 0; i < num_artists_; i++) {
-    artists_[i] = other_song.artists_[i];
+  // Default Constructor
+  SongRecording::SongRecording(const string& title,
+      const string& primary_artist, int duration, int num_artists) :
+      title_(title), num_artists_(num_artists > 0 ? num_artists : 1),
+      duration_(duration) {
+    artists_ = new string[num_artists_];
+    SetArtist(primary_artist);
+    for (int i = 1; i < num_artists_; i++) {
+      artists_[i] = "unknown";
+    }
   }
 
-}
-
-// Overloaded Assignment Operator
-SongRecording& SongRecording::operator=(const SongRecording& other_song) {
-  if (this != &other_song) {
-    delete[] artists_;
-    title_ = other_song.title_;
-    num_artists_ = other_song.num_artists_;
-    duration_ = other_song.duration_;
+  // Copy Constructor
+  SongRecording::SongRecording(const SongRecording& other_song) :
+    title_(other_song.title_), num_artists_(other_song.num_artists_),
+    duration_(other_song.duration_) {
     artists_ = new string[num_artists_];
     for (int i = 0; i < num_artists_; i++) {
       artists_[i] = other_song.artists_[i];
     }
   }
-  return *this;
-}
 
-// Destructor
-SongRecording::~SongRecording() {
-  delete[] artists_;
-}
-
-// Getters
-string SongRecording::GetArtist(const int index) const {
-  if (index < 1 || index > num_artists_) {
-    return "out of bounds";
+  // Overloaded Assignment Operator
+  SongRecording& SongRecording::operator=(const SongRecording& other_song) {
+    if (this != &other_song) {
+      delete[] artists_;
+      title_ = other_song.title_;
+      num_artists_ = other_song.num_artists_;
+      duration_ = other_song.duration_;
+      artists_ = new string[num_artists_];
+      for (int i = 0; i < num_artists_; i++) {
+        artists_[i] = other_song.artists_[i];
+      }
+    }
+    return *this;
   }
-  if (artists_[index - 1].length() == 0) {
-    return "unknown";
-  }
-  return artists_[index - 1];
-}
 
-// Setters
-void SongRecording::SetTitle(const string title) {
-  if (title.length() > 0) {
-      title_ = title;
-  }
-}
-
-void SongRecording::SetNumArtists(const int num_artists) {
-  if (num_artists > 0) {
-    num_artists_ = num_artists;
+  // Destructor
+  SongRecording::~SongRecording() {
     delete[] artists_;
-    artists_ = new string[num_artists_];
   }
-}
 
-void SongRecording::SetArtist(const string& artist_name, int index) {
-  if (index < 1 || index > num_artists_ 
-      || artist_name.length() == 0) {
-    return;
+  // Getters
+  string SongRecording::GetArtist(const int index) const {
+    if (index < 1 || index > num_artists_) {
+      return "out of bounds";
+    }
+    if (artists_[index - 1].length() == 0) {
+      return "unknown";
+    }
+    return artists_[index - 1];
   }
-  artists_[index - 1] = artist_name;
-}
 
-void SongRecording::SetTrackLength(const int duration) {
-  if (duration > 0) {
-    duration_ = duration;
+  // Setters
+  void SongRecording::SetTitle(const string title) {
+    if (title.length() > 0) {
+        title_ = title;
+    }
   }
-}
 
-}  // namespace cscs240_programming_assignment_5
+  void SongRecording::SetNumArtists(const int num_artists) {
+    if (num_artists > 0) {
+      num_artists_ = num_artists;
+      delete[] artists_;
+      artists_ = new string[num_artists_];
+    }
+  }
+
+  void SongRecording::SetArtist(const string& artist_name, int index) {
+    if (index < 1 || index > num_artists_
+        || artist_name.length() == 0) {
+      return;
+    }
+    artists_[index - 1] = artist_name;
+  }
+
+  void SongRecording::SetTrackLength(const int duration) {
+    if (duration > 0) {
+      duration_ = duration;
+    }
+  }
+
+}  // namespace csce240_programming_assignment_5
--- Homework 5/SongRecording.h
+++ Homework 5/SongRecording.h
@@ -8,39 +8,34 @@ using std::string;
 
 namespace csce240_programming_assignment_5 {
 
- class SongRecording {
-  private:
-   string title_;
-   string * artists_;
-   int num_artists_;
-   int duration_;
- 
-  public:
-   explicit SongRecording(const string& title = "untitled",
-    const string& primary_artist = "unknown",
-    int duration = 0, int num_artists = 1);
-   
-   // Copy Constructor
-   SongRecording(const SongRecording& other_song);
- 
-   // Overloaded Assignment Operator
-   SongRecording& operator=(const SongRecording& other_song);
- 
-   // Destructor
-   ~SongRecording();
-   
-   // Getters
-   string GetTitle() const{return title_;}
-   int GetNumArtists() const{return num_artists_;}
-   string GetArtist(const int index = 1) const;
-   int GetTrackLength() const{return duration_ < 0 ? 0 : duration_;}
- 
-   // Setters
-   void SetTitle(const string title);
-   void SetNumArtists(const int num_artists);
-   void SetArtist(const string& artist_name, int index = 1);
-   void SetTrackLength(const int duration);
- };
-} // namespace cscs240_programming_assignment_5
+class SongRecording {
+ private:
+  string title_;
+  string * artists_;
+  int num_artists_;
+  int duration_;
+
+ public:
+  explicit SongRecording(const string& title = "untitled",
+   const string& primary_artist = "unknown",
+   int duration = 0, int num_artists = 1);
+  // Copy Constructor
+  SongRecording(const SongRecording& other_song);
+  // Overloaded Assignment Operator
+  SongRecording& operator=(const SongRecording& other_song);
+  // Destructor
+  ~SongRecording();
+  // Getters
+  string GetTitle() const {return title_;}
+  int GetNumArtists() const {return num_artists_;}
+  string GetArtist(const int index = 1) const;
+  int GetTrackLength() const {return duration_ < 0 ? 0 : duration_;}
+  // Setters
+  void SetTitle(const string title);
+  void SetNumArtists(const int num_artists);
+  void SetArtist(const string& artist_name, int index = 1);
+  void SetTrackLength(const int duration);
+};
+}  // namespace csce240_programming_assignment_5
 
 #endif  // SONGRECORDING_H_
